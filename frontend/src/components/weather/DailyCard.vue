01d5021824b3fcd100af08f1cb55327716198bd7
Dudoladov Dmitry <dudmitry.iv@gmail.com>
2025-11-03 21:10:05 +0300

    feat(ui): create DailyCard component

---
+++ frontend/src/components/weather/DailyCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div :key="props.day.date" class="day-card">
+        <p><strong>{{ formatDate(props.day.date, props.lang) }}</strong></p>
+        <img :src="`https://openweathermap.org/img/wn/${props.day.icon}.png`" :alt="props.day.description">
+        <p>{{ props.day.min_temp }}° / {{ props.day.max_temp }}°</p>
+        <p>{{ props.day.description }}</p>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(["day", "lang"]);
+import { formatDate } from '@/utils/weatherUtil';
+</script>
+
+<style scoped>
+.day-card {
+    background: white;
+    padding: 15px;
+    border-radius: 6px;
+    text-align: center;
+}
+
+.day-card p {
+    margin: 5px 0;
+}
+</style>
